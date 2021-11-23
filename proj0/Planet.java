555e682e5a0f079912e4f595f502bdc5273f4188
HongxiChen1014 <hongxi_chen2020@163.com>
2021-11-23 00:40:48 -0800

    finished

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,13 +4,13 @@
  * @create 2021-11-22-21:34
  */
 public class Planet {
-    double G = 6.67e-11;
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double G = 6.67e-11;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
