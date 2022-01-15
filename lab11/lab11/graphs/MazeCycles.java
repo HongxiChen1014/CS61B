2c174c6f6da6f601ea6f8851acb20b6e02c34240
HongxiChen1014 <hongxi_chen2020@163.com>
2022-01-14 16:45:36 -0800

    finished

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +11,53 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] edgeCopy;
+    private boolean findCycle = false;
 
     public MazeCycles(Maze m) {
         super(m);
     }
 
+
     @Override
     public void solve() {
-        // TODO: Your code here!
+        edgeCopy = new int[maze.V()];
+        Stack<Integer> fringe = new Stack<>();
+        // start from 0
+        fringe.push(0);
+        while (!fringe.isEmpty()) {
+            if (findCycle) {
+                break;
+            }
+            int v = fringe.pop();
+            marked[v] = true;
+            announce();
+            for (int w : maze.adj(v)) {
+                if (marked[w] && edgeCopy[v] != w) {
+                    edgeCopy[w] = v;
+                    drawCycle(w);
+                    findCycle = true;
+                    break;
+                } else if (!marked[w]) {
+                    edgeCopy[w] = v;
+                    fringe.push(w);
+                }
+            }
+        }
     }
 
+
     // Helper methods go here
+    private void drawCycle(int start) {
+        int vertex = start;
+        while (true) {
+            edgeTo[vertex] = edgeCopy[vertex];
+            vertex = edgeCopy[vertex];
+            if (vertex == start) {
+                break;
+            }
+        }
+        announce();
+    }
 }
 
